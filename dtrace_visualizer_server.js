6380ce938306cf00a500d58546484152ec53ab5d
Geoff Flarity <geoff@feather.local>
2011-02-06 15:32:18 -0500

    corrected grammar

--- dtrace_visualizer_server.js
+++ dtrace_visualizer_server.js
@@ -4,7 +4,7 @@ var io = require('socket.io');
 var express = require('express');
 
 
-/* create our express server and give access to the javascript files in ./public 
+/* create our express server and prepare to serve javascript files in ./public 
 */
 var app = express.createServer();
 app.configure(function(){
@@ -14,15 +14,15 @@ app.listen(80);
 
 
 /* Before we go any further we must realize that each time a user connects we're going to want to 
-   them send then Dtrace data every second. We can do so using 'setInterval', but we must keep 
-   track of both the intervals we set and the Dtrace handles that are created inside of them as 
-   we'll need them later when the client disconnects. 
+   them send them dtrace aggregation every second. We can do so using 'setInterval', but we must
+   keep track of both the intervals we set and the dtrace consumers that are created as we'll need 
+   them later when the client disconnects. 
 */
 var interval_id_by_session_id = {};
 var dtp_by_session_id = {};
 
 /* In order to effecienctly send packets we're going to use the Socket.IO library which seemlessly 
-   integrates with express. 
+   integrates with express.  
 */
 var websocket_server = io.listen(app); 
 
@@ -32,10 +32,9 @@ var websocket_server = io.listen(app);
 socket.on('connection', function(socket) { 
 
 	/* Like the web server object, we must also define handlers for various socket events that 
-	   will happen during the lifetime of the connection. This will define how we interact with
+	   will happen during the lifetime of the connection. These will define how we interact with
            the client. The first is a message event which occurs when the client sends something to
 	   the server. */
-
 	socket.on( 'message', function(message) { 
 		/* The only message the client ever sends will be sent right after connecting.  
                    So it will happen only once during the lifetime of a socket. This message also 
@@ -69,17 +68,14 @@ socket.on('connection', function(socket) {
 	    } );
 	    
 
-	/* Not so fast. If a client disconnects we don't want we don't want dtrace to keep 
-	   collecting data any more. We also don't want to try to keep sending period. Clean up. */
+	/* Not so fast. If a client disconnects we don't want their respective dtrace consumer to 
+	   keep collecting data any more. We also don't want to try to keep sending anything to them
+	   period. So clean up. */
 	client.on('disconnect', function(){ 
-		//stop sending the data
 		clearInterval(clearInterval(interval_id_by_session_id[client.sessionId]));
 		var dtp = dtp_by_session_id[client.sessionId];
-		//should be only other reference to this should die once out of scope. 
 		delete dtp_by_session_id[client.sessionId]; 
-		dtp.stop();
-
-		
+		dtp.stop();		
 		console.log('disconnected');
 	    });
 	
